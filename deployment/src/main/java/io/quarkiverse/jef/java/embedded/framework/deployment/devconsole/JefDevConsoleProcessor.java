f3c41fad9705c14eb9173e5937285de54dce5098
Alexey Sharandin <sanders@yandex.ru>
2023-09-23 14:11:47 +0300

    hide old dev console

--- deployment/src/main/java/io/quarkiverse/jef/java/embedded/framework/deployment/devconsole/JefDevConsoleProcessor.java
+++ deployment/src/main/java/io/quarkiverse/jef/java/embedded/framework/deployment/devconsole/JefDevConsoleProcessor.java
@@ -5,36 +5,36 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import io.quarkiverse.jef.java.embedded.framework.runtime.dev.JefDevConsoleRecorder;
 import io.quarkiverse.jef.java.embedded.framework.runtime.dev.tracing.*;
 import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
-import io.quarkus.deployment.annotations.ExecutionTime;
-import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.nativeimage.ServiceProviderBuildItem;
-import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
+
 //import io.quarkus.devconsole.spi.DevConsoleRuntimeTemplateInfoBuildItem;
 
 @SuppressWarnings("unused")
 public class JefDevConsoleProcessor {
     private final static Logger logger = LogManager.getLogger("JEF-Dev-Tools");
 
-/*    @BuildStep(onlyIf = IsDevelopment.class)
-    @Record(ExecutionTime.RUNTIME_INIT)
-    public void exposeJefContainer(
-            BuildProducer<DevConsoleRuntimeTemplateInfoBuildItem> producer,
-            CurateOutcomeBuildItem curateOutcomeBuildItem,
-            JefDevConsoleRecorder recorder) {
-        //Engine engine = createEngine();
-        //Router
-        logger.debug("Apply Jef Dev Console Recorder to Quarkus build system");
-        producer.produce(new DevConsoleRuntimeTemplateInfoBuildItem(
-                "io.quarkiverse.jef",
-                "quarkus-java-embedded-framework",
-                "jefDevContainer",
-                recorder.getSupplier()));
-    }*/
+    /*
+     * @BuildStep(onlyIf = IsDevelopment.class)
+     *
+     * @Record(ExecutionTime.RUNTIME_INIT)
+     * public void exposeJefContainer(
+     * BuildProducer<DevConsoleRuntimeTemplateInfoBuildItem> producer,
+     * CurateOutcomeBuildItem curateOutcomeBuildItem,
+     * JefDevConsoleRecorder recorder) {
+     * //Engine engine = createEngine();
+     * //Router
+     * logger.debug("Apply Jef Dev Console Recorder to Quarkus build system");
+     * producer.produce(new DevConsoleRuntimeTemplateInfoBuildItem(
+     * "io.quarkiverse.jef",
+     * "quarkus-java-embedded-framework",
+     * "jefDevContainer",
+     * recorder.getSupplier()));
+     * }
+     */
 
     /*
      * @BuildStep(onlyIf = IsDevelopment.class)
